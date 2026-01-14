f00982fc48f7e22f38d40b0b4f30fef066ee2964
Roshan Kumar Gupta <159025510+roshan7187@users.noreply.github.com>
2026-01-15 02:09:56 +0530

    Time: 7 ms (82.77%) | Memory: 19.8 MB (21.69%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,15 +1,17 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        
+        l, r = 0, len(s) - 1
         s = s.lower()
-        str =""
-        
-        for ch in s :
-            if '0' <= ch <='9' or 'a' <= ch <= 'z' :
-                str+=ch
-        n = len(str)
-        for ind in range(0,n//2):
-            if str[ind] != str[-ind-1] :
 
+        while l < r:
+            if not ('a' <= s[l] <= 'z' or '0' <= s[l] <= '9'):
+                l += 1
+            elif not ('a' <= s[r] <= 'z' or '0' <= s[r] <= '9'):
+                r -= 1
+            elif s[l] != s[r]:
                 return False
+            else:
+                l += 1
+                r -= 1
+
         return True
