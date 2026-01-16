4a508915087eeb3efd6eb930e93ce86d668bfea0
Roshan Kumar Gupta <159025510+roshan7187@users.noreply.github.com>
2026-01-16 18:31:43 +0530

    Time: 865 ms (19.81%) | Memory: 22.3 MB (10.77%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -17,13 +17,11 @@ class Solution:
                     triplet = [nums[index] , nums[left] , nums[right]]
                     ans.append(triplet)
                     left+=1
-                    right-=1
 
                     while left < right and nums[left] == nums[left - 1]:
                         left += 1
 
-                    while left < right and nums[right] == nums[right + 1]:
-                        right -= 1
+                    
 
                 elif nums[index] + nums[left] + nums[right] > 0 :
                     right-=1
