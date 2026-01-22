407af0feb612647c347736dbc785da90f384fbd4
Roshan Kumar Gupta <159025510+roshan7187@users.noreply.github.com>
2026-01-22 19:51:51 +0530

    Time: 11 ms (96.95%) | Memory: 19.4 MB (15.83%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -4,19 +4,22 @@ class Solution:
         if len(s1) > len(s2) :
             return False
 
-        need = Counter(s1)
-        window = Counter(s2[:len(s1)])
+        need = [0]*26
+        window = [0]*26
+
+        for ch in s1 :
+            need[ord(ch) - ord('a')] += 1
+        
+        for ch in s2[:len(s1)] :
+            window[ord(ch) - ord('a')] +=1
 
         if window == need :
             return True
         
         left = 0
         for right in range(len(s1),len(s2)) :
-            window[s2[right]] +=1
-            window[s2[left]] -=1
-
-            if window[s2[left] == 0] :
-                del window[s2[left]]
+            window[ord(s2[right]) - ord('a')] +=1
+            window[ord(s2[left]) - ord('a')] -=1
             left+=1
 
             if window == need :
