ff5dd20d5caf9fde43ac55a7b44dff4ec6f68faa
Roshan Kumar Gupta <159025510+roshan7187@users.noreply.github.com>
2026-01-23 17:30:28 +0530

    Time: 185 ms (32.34%) | Memory: 20.1 MB (5.04%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -4,23 +4,21 @@ class Solution:
         if len(p) > len(s) :
             return []
 
-        need = [0]*26
-        window = [0]*26
+        need = Counter(p)
+        window = Counter(s[:len(p)])
         ans = []
         left = 0
-        for ch in p :
-            need[ord(ch) - ord('a')] += 1
-        
-        for ch in s[:len(p)] :
-            window[ord(ch) - ord('a')] +=1
 
         if window == need :
             ans.append(left)
 
         
         for right in range(len(p),len(s)) :
-            window[ord(s[right]) - ord('a')] +=1
-            window[ord(s[left]) - ord('a')] -=1
+            window[s[right]] +=1
+            window[s[left]] -=1
+
+            if window[s[left]] == 0 :
+                del window[s[left]]
             left+=1
 
             if window == need :
